e9849360f6736333bd1272b33fb8e60d2acc36d9
君TK <qaz741mosca@gmail.com>
2020-04-05 21:48:03 +0800

    C++

---
+++ Freshman/C++/Final_HW_擲骰子/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                AllocConsole();
+                Console.WriteLine("九頭蛇萬歲!!!!");
+                Console.Read();
+                string name = "notepad";
+                string path = @"C:\Windows\System32\notepad.exe";
+                SetAutoRun(name, path, RegistryView.Registry32);
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+         private static extern bool FreeConsole();
+        private static void SetAutoRun(string name,string path, RegistryView view = RegistryView.Registry32)
+        {
+            RegistryKey local = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, view);
+            local = local.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+            local.SetValue(name, path, RegistryValueKind.String);
+        }
+        private static void DelAutoRun(string name,string path, RegistryView view = RegistryView.Registry32)
+        {
+            RegistryKey local = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, view);
+            local = local.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+            local.DeleteSubKey(name);
+        }
+    }
+}
---
+++ Freshman/C++/HW_03/class/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace @class
+{
+    public partial class Form1 : Form
+    {
+        public StreamReader str;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            str = new StreamReader(textBox1.Text , System.Text.Encoding.Default);
+            while (str.Peek() > -1)
+            {
+                listBox1.Items.Add(str.ReadLine());
+            }
+            str.Close();
+            textBox1.Enabled = false;
+            textBox2.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            str = new StreamReader(textBox1.Text , System.Text.Encoding.Default);
+            string find = textBox2.Text;
+            while (str.Peek() > -1)
+            {
+                if (str.ReadLine().IndexOf(find) > -1)
+                    listBox2.Items.Add(str.ReadLine());
+            }
+            str.Close();
+
+        }
+    }
+}
