6db3e4314db05b7ae14c5777e2728cd65260d51c
君TK <qaz741mosca@gmail.com>
2020-04-26 19:06:45 +0800

    Unity 20200414 課堂實作

---
+++ Junior/Unity/20200303_1/Assets/NPC_move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPC_move : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speed;
+    int cur = 0;
+    public GameObject R2_object;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        R2_object.GetComponent<Animator>().SetBool("burning", false);
+
+        if(transform.position != waypoints[cur].position)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        else
+            cur = (cur + 1) % waypoints.Length;
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if(collision.tag == "R2")
+        {
+            R2_object.GetComponent<Animator>().SetBool("burning", true);
+        }
+	}
+}
+
---
+++ Junior/Unity/20200303_1/Assets/fruit_OnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruit_OnTrigger : MonoBehaviour
+{
+    static AudioClip colsound;
+    public AudioSource sound;
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D co_target)
+    {
+        if (co_target.tag == "R2")
+        {
+            sound.PlayOneShot(colsound);
+            sound.Play();
+            score = score + 10;
+            Debug.Log("Score: " + score);
+            Destroy(gameObject);
+        }
+    }
+}
