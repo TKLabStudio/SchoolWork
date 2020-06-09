c5404962119b1fd3349b2150676e99d27c472b23
君TK <qaz741mosca@gmail.com>
2020-06-09 23:24:15 +0800

    Unity 20200602 課堂實作

---
+++ Junior/Unity/20200303_1/Assets/Move_left.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_left : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + new Vector3(-10.24f / 20 * Time.deltaTime * 2, 0, 0);
+	}
+}
---
+++ Junior/Unity/20200303_1/Assets/stone_blasting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stone_blasting : MonoBehaviour
+{
+    Animator m_Animator;
+    AudioSource blastering_sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        blastering_sound = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D co_target)
+    {
+        if (co_target.tag == "X_ray")
+        {
+            m_Animator.SetTrigger("blasting");
+            blastering_sound.Play();
+
+        }
+    }
+    void destory()
+    {
+        Destroy(gameObject);
+    }
+
+}
