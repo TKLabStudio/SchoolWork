1a7b2acc32dd6ed72dc548c2db120ddfe99557ab
君TK <qaz741mosca@gmail.com>
2020-06-10 10:58:06 +0800

    Unity 20200609 課堂實作

---
+++ Junior/Unity/DeityDefence_Resource/Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject sensor;
+    public GameObject enemy;
+    public GameObject enemy_cur_pos;
+    public int time = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            player.GetComponent<Animator>().SetBool("atk", true);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(sensor.GetComponent<Collider>().Raycast(ray, out hit, 600))
+            {
+                Debug.DrawRay(ray.origin, hit.point);
+                player.transform.LookAt(hit.point);
+            }
+        }else{
+            player.GetComponent<Animator>().SetBool("atk", false);
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            Vector3 max = enemy_cur_pos.GetComponent<Collider>().bounds.max;
+            Vector3 min = enemy_cur_pos.GetComponent<Collider>().bounds.min;
+            float z = Random.Range(min.x, max.z);
+            Vector3 arae = new Vector3(min.x, min.y, z);
+            GameObject eney = (GameObject)GameObject.Instantiate(enemy, enemy_cur_pos.transform.position, enemy_cur_pos.transform.rotation);
+        }
+    }
+}
---
+++ Junior/Unity/DeityDefence_Resource/Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject firepoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void fire()
+    {
+        GameObject bullte = (GameObject)GameObject.Instantiate(bullet, firepoint.transform.position, firepoint.transform.rotation);
+    }
+}
